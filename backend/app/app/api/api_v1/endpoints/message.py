20f2b8cd4760b5da26724a74c292a0fb5a500ece
Iuri <iurii84@gmail.com>
2022-06-23 21:00:04 -0400

    Message uuid validation

--- backend/app/app/api/api_v1/endpoints/message.py
+++ backend/app/app/api/api_v1/endpoints/message.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+import uuid
+from fastapi import APIRouter, HTTPException
 
 from app import schemas
 
@@ -7,5 +8,9 @@ router = APIRouter()
 
 @router.post("", response_model=schemas.Message)
 def send_message(msg: schemas.Message):
+    try:
+        uuid.UUID(msg.uuid)
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Wrong uuid")
     print(msg)
     return msg
--- backend/app/app/schemas/message.py
+++ backend/app/app/schemas/message.py
@@ -1,10 +1,12 @@
-from typing import Optional
+from typing import Optional, Union
 
 from pydantic import BaseModel, Field
 
 
 class MessageBase(BaseModel):
-    item: str
+    uuid: str
+    temp: Union[None, float]
+    hum: Union[None, float]
 
 
 class Message(MessageBase):
