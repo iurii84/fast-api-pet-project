6e0c0d732ec272388dea74ebed8110236a5865d2
Iuri <iurii84@gmail.com>
2022-06-26 17:41:43 -0400

    get messages

--- backend/app/app/api/api_v1/endpoints/message.py
+++ backend/app/app/api/api_v1/endpoints/message.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
@@ -14,3 +14,11 @@ def send_message(msg: schemas.Message, db: Session = Depends(deps.get_db)) -> An
     message = crud.message.create(db, obj_in=msg)
     print(msg)
     return message
+
+
+@router.get("", response_model=List[schemas.MessageInDb])
+def get_messages(db: Session = Depends(deps.get_db),
+                 skip: int = 0,
+                 limit: int = 100) -> Any:
+    messages = crud.message.get_multi(db, skip=skip, limit=limit)
+    return messages
