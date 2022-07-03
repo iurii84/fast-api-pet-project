0fa4457e552b39675aa65727b02a50503bd84592
Iuri <iurii84@gmail.com>
2022-07-03 16:27:53 -0400

    FE: timeline range in chart

--- backend/app/app/api/api_v1/endpoints/message.py
+++ backend/app/app/api/api_v1/endpoints/message.py
@@ -19,8 +19,14 @@ def send_message(msg: schemas.Message, db: Session = Depends(deps.get_db)) -> An
 @router.get("", response_model=List[schemas.MessageInDb])
 def get_messages(db: Session = Depends(deps.get_db),
                  skip: int = 0,
-                 limit: int = 100,
+                 limit: int = 300000,
                  order_by: str = 'id',
-                 after_id: int = 0) -> Any:
-    messages = crud.message.get_multi(db, skip=skip, limit=limit, order_by=order_by, after_id=after_id)
+                 after_id: int = 0,
+                 lastMinutes: int = 0) -> Any:
+    messages = crud.message.get_multi(db,
+                                      skip=skip,
+                                      limit=limit,
+                                      order_by=order_by,
+                                      after_id=after_id,
+                                      lastMinutes=lastMinutes)
     return messages
--- backend/app/app/crud/crud_message.py
+++ backend/app/app/crud/crud_message.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import List
 
 from sqlalchemy import func
@@ -22,12 +22,23 @@ class CRUDMessage(CRUDBase[Message, Message, Message]):
         return db_obj
 
     def get_multi(
-            self, db: Session, *, skip: int = 0, limit: int = 100, order_by: str = "id", after_id: int = 0
+            self, db: Session, *,
+            skip: int = 0,
+            limit: int = 300000,
+            order_by: str = "id",
+            after_id: int = 0,
+            lastMinutes: int = 0
     ) -> List[Message]:
         if order_by == 'id':
             if after_id > 0:
                 return db.query(self.model).where(self.model.id > after_id).order_by(Message.id).offset(skip). \
                     limit(limit).all()
+            else:
+                if lastMinutes > 0:
+                    date_time_now = datetime.now()
+                    date_time_result = date_time_now - timedelta(minutes=lastMinutes)
+                    return db.query(self.model).where(self.model.created > date_time_result).order_by(Message.id).\
+                        offset(skip).limit(limit).all()
             message_id = db.query(func.max(self.model.id)).first()[0] - 1000
 
             return db.query(self.model).where(self.model.id > message_id).order_by(Message.id).offset(skip).limit(limit).all()
