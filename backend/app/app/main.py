57c805fdceb51e788bb20933106794630e446bae
Iuri <iurii84@gmail.com>
2022-08-02 20:15:12 -0400

    BE - origins CORS

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -8,14 +8,26 @@ app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://192.168.88.254/",
+    "http://192.168.88.254",
+    "172.22.0.*",
+    "http://172.22.0.*",
+    "http://172.22.0.*:80"
+]
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
