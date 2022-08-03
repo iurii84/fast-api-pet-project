9e933aea3957771d6f1de68f58f355b54c453de8
Iuri <iurii84@gmail.com>
2022-08-02 20:17:56 -0400

    BE - cleanup

--- backend/app/app/models/message.py
+++ backend/app/app/models/message.py
@@ -1,7 +1,6 @@
-#from fastapi_utils.guid_type import GUID
 from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.dialects import postgresql
-from sqlalchemy.dialects.postgresql import UUID
+
 
 from app.db.base_class import Base
 
@@ -11,7 +10,6 @@ from app.db.base_class import Base
 
 class Message(Base):
     id = Column(Integer, primary_key=True, index=True)
-    # uuid = Column(GUID)
     uuid = Column()
     temp = Column(Float, index=True)
     hum = Column(Float, index=True)
