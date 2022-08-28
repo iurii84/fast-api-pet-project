a392c23813d75971cc3185af6194db5657289982
Iuri <iurii84@gmail.com>
2022-08-28 00:10:10 -0400

    BE add new tables: sensor_location, sensor_type

--- backend/app/app/models/__init__.py
+++ backend/app/app/models/__init__.py
@@ -2,3 +2,5 @@ from .item import Item
 from .user import User
 from .message import Message
 from .sensor import Sensor
+from .sensor_location import SensorLocation
+from .sensor_type import SensorType
---
+++ backend/app/app/models/sensor_type.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.dialects import postgresql
+
+from app.db.base_class import Base
+
+# if TYPE_CHECKING:
+#     from .message import Message  # noqa: F401
+
+
+class SensorType(Base):
+    type_id = Column(Integer, primary_key=True, index=True)
+    name = Column(postgresql.VARCHAR(50), nullable=False)
+    description = Column(postgresql.TEXT, nullable=True)
+
