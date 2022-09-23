8944f43672e5d45dab08d7b4a7a93ef8a830da11
Iuri <iurii84@gmail.com>
2022-09-23 19:07:22 -0400

    BE - device_data_bind finish

--- backend/app/app/api/api_v1/endpoints/device_data_bind.py
+++ backend/app/app/api/api_v1/endpoints/device_data_bind.py
@@ -1,10 +1,11 @@
 from typing import Any, List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app import schemas, crud
 from app.api import deps
+from app.schemas import DeviceDataBindBase
 
 router = APIRouter()
 
@@ -17,3 +18,44 @@ async def get_device_data_binds(db: Session = Depends(deps.get_db),
                                                         skip=skip,
                                                         limit=limit)
     return device_data_binds
+
+
+@router.post("")
+async def post_device_data_bind(*, msg: schemas.RegisterDeviceDataBind, db: Session = Depends(deps.get_db)) -> Any:
+    device_data_bind = crud.device_data_bind.create(db=db, obj_in=msg)
+    return device_data_bind
+
+
+@router.patch("/{binder_id}", response_model=DeviceDataBindBase)
+async def patch_device_data_bind(
+        *,
+        msg: schemas.PatchDeviceDataBind,
+        db: Session = Depends(deps.get_db),
+        binder_id: int,
+) -> Any:
+    """
+    Partial update device.
+    """
+    existing_device_data_binder = crud.device_data_bind.get(db=db, id=binder_id)
+    if not existing_device_data_binder:
+        raise HTTPException(status_code=404, detail="Binder not found")
+
+    device_data_binder_updated = crud.device_data_bind.update(db=db, db_obj=existing_device_data_binder, obj_in=msg)
+    return device_data_binder_updated
+
+
+@router.delete("/{binder_id}", response_model=schemas.DeleteDeviceReturn)
+def delete_device_data_bind(
+        *,
+        db: Session = Depends(deps.get_db),
+        binder_id: int,
+) -> Any:
+    """
+    Delete device.
+    """
+    binder = crud.device_data_bind.get(db=db, id=binder_id)
+    if not binder:
+        raise HTTPException(status_code=404, detail="Binder not found")
+
+    binder = crud.device_data_bind.remove(db=db, id=binder_id)
+    return binder
--- backend/app/app/schemas/device_data_bind.py
+++ backend/app/app/schemas/device_data_bind.py
@@ -13,3 +13,19 @@ class DeviceDataBindBase(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class RegisterDeviceDataBind(BaseModel):
+    binder_name: str
+    device_id: int
+    device_prop: str
+    subscriber_id: int
+    char_placeholder: int
+
+
+class PatchDeviceDataBind(BaseModel):
+    binder_name: Union[None, str]
+    device_id: Union[None, int]
+    device_prop: Union[None, str]
+    subscriber_id: Union[None, int]
+    char_placeholder: Union[None, int]
