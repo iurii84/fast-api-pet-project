b2d53f5db3e1c8e5918864c3a8abbc776a28adb8
Iuri <iurii84@gmail.com>
2022-10-26 21:57:02 -0400

    FE - upgrade axios from 0.18.0 to 1.1.3

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,23 +1,13 @@
-import axios from 'axios';
+const axios = require('axios').default;
 import { apiUrl } from '@/env';
 import {
-  IDeviceAvailableList,
-  IDeviceNotRegisteredList,
-  IUserProfile,
+  
   IUserProfileUpdate,
   IUserProfileCreate,
   ICompressDbPayload,
-  ICompressResponse,
-  IDeviceRegisterResponse,
   IDeviceRegisterPost,
-  IDeviceLocationResponse,
-  IDeviceTypeResponse,
   IDeviceUpdate,
-  ITaskGetResponse,
-  IDeviceDataBindList,
-  IDeviceGetParams,
   IDataBingRegisterPost,
-  IDataBingRegisterResponse,
   IDatabindUpdatePatch,
 } from './interfaces';
 
@@ -31,46 +21,56 @@ function authHeaders(token: string) {
 
 export const api = {
   async updateDatabind(token: string, data: IDatabindUpdatePatch, deviceId: number) {
-    return axios.patch(`${apiUrl}/api/v1/device_data_bind/${deviceId}`, data);
+    return await axios.patch(`${apiUrl}/api/v1/device_data_bind/${deviceId}`, data);
   },
   async deleteDatabind(token: string, deviceId: number) {
-    return axios.delete(`${apiUrl}/api/v1/device_data_bind/${deviceId}`);
+    return await axios.delete(`${apiUrl}/api/v1/device_data_bind/${deviceId}`);
   },
   async registerDataBind(token: string, data: IDataBingRegisterPost) {
-    return axios.post<IDataBingRegisterResponse>(`${apiUrl}/api/v1/device_data_bind`, data);
+    let response = await axios.post(`${apiUrl}/api/v1/device_data_bind`, data);
+    return response
   },
   async getDeviceParams(device_uuid: string) {
-    return axios.get<IDeviceGetParams>(`${apiUrl}/api/v1/device/get_params_by_uuid/${device_uuid}`);
+    let response = await axios.get(`${apiUrl}/api/v1/device/get_params_by_uuid/${device_uuid}`);
+    return response
   },
   async getDeviceDataBindList() {
-    return axios.get<IDeviceDataBindList[]>(`${apiUrl}/api/v1/device_data_bind`);
+    let response = await axios.get(`${apiUrl}/api/v1/device_data_bind`);
+    return response
   },
   async updateDevice(token: string, data: IDeviceUpdate, deviceId: number) {
-    return axios.patch(`${apiUrl}/api/v1/device/${deviceId}`, data);
+    return await axios.patch(`${apiUrl}/api/v1/device/${deviceId}`, data);
   },
   async deleteDevice(token: string, deviceId: number) {
-    return axios.delete(`${apiUrl}/api/v1/device/${deviceId}`);
+    return await axios.delete(`${apiUrl}/api/v1/device/${deviceId}`);
   },
   async getDeviceTypes() {
-    return axios.get<IDeviceTypeResponse[]>(`${apiUrl}/api/v1/device_type`);
+    let response = await axios.get(`${apiUrl}/api/v1/device_type`);
+    return response
   },
   async getDeviceLocations() {
-    return axios.get<IDeviceLocationResponse[]>(`${apiUrl}/api/v1/device_location`);
+    let response = await axios.get(`${apiUrl}/api/v1/device_location`);
+    return response
   },
   async registerDevice(token: string, data: IDeviceRegisterPost) {
-    return axios.post<IDeviceRegisterResponse>(`${apiUrl}/api/v1/device/register`, data);
+    let response = await axios.post(`${apiUrl}/api/v1/device/register`, data);
+    return response
   },
   async getDevices(is_display: boolean = false) {
-    return axios.get<IDeviceAvailableList[]>(`${apiUrl}/api/v1/device`, { params: { is_display } });
+    let response = await axios.get(`${apiUrl}/api/v1/device`, { params: { is_display } });
+    return response
   },
   async getNotRegisteredDevices() {
-    return axios.get<IDeviceNotRegisteredList[]>(`${apiUrl}/api/v1/device/get_unregistered`);
+    let response = await axios.get(`${apiUrl}/api/v1/device/get_unregistered`);
+    return response
   },
   async getTask(token: string, task_id: string) {
-    return axios.get<ITaskGetResponse>(`${apiUrl}/api/v1/task/`, { params: { task_id } });
+    let response = await axios.get(`${apiUrl}/api/v1/task/`, { params: { task_id } });
+    return response
   },
   async compressDb(token: string, data: ICompressDbPayload) {
-    return axios.post<ICompressResponse>(`${apiUrl}/api/v1/message/compress`, data);
+    let response = await axios.post(`${apiUrl}/api/v1/message/compress`, data);
+    return response
   },
 
   async logInGetToken(username: string, password: string) {
@@ -78,28 +78,31 @@ export const api = {
     params.append('username', username);
     params.append('password', password);
 
-    return axios.post(`${apiUrl}/api/v1/login/access-token`, params);
+    return await axios.post(`${apiUrl}/api/v1/login/access-token`, params);
   },
   async getMe(token: string) {
-    return axios.get<IUserProfile>(`${apiUrl}/api/v1/users/me`, authHeaders(token));
+    let response = await axios.get(`${apiUrl}/api/v1/users/me`, authHeaders(token));
+    return response
   },
   async updateMe(token: string, data: IUserProfileUpdate) {
-    return axios.put<IUserProfile>(`${apiUrl}/api/v1/users/me`, data, authHeaders(token));
+    let response = await axios.put(`${apiUrl}/api/v1/users/me`, data, authHeaders(token));
+    return response
   },
   async getUsers(token: string) {
-    return axios.get<IUserProfile[]>(`${apiUrl}/api/v1/users/`, authHeaders(token));
+    let response = await axios.get(`${apiUrl}/api/v1/users/`, authHeaders(token));
+    return response
   },
   async updateUser(token: string, userId: number, data: IUserProfileUpdate) {
-    return axios.put(`${apiUrl}/api/v1/users/${userId}`, data, authHeaders(token));
+    return await axios.put(`${apiUrl}/api/v1/users/${userId}`, data, authHeaders(token));
   },
   async createUser(token: string, data: IUserProfileCreate) {
-    return axios.post(`${apiUrl}/api/v1/users/`, data, authHeaders(token));
+    return await axios.post(`${apiUrl}/api/v1/users/`, data, authHeaders(token));
   },
   async passwordRecovery(email: string) {
-    return axios.post(`${apiUrl}/api/v1/password-recovery/${email}`);
+    return await axios.post(`${apiUrl}/api/v1/password-recovery/${email}`);
   },
   async resetPassword(password: string, token: string) {
-    return axios.post(`${apiUrl}/api/v1/reset-password/`, {
+    return await axios.post(`${apiUrl}/api/v1/reset-password/`, {
       new_password: password,
       token,
     });
